dd22385c3ffa5c991e98328223908a5c7d9aa11b
dayub@chinasystems.com <dayu@DELL-Precision3630.localdomain>
2020-07-06 13:44:56 +0800

    Move java-spring-jaeger configuration to k8s deployment

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
